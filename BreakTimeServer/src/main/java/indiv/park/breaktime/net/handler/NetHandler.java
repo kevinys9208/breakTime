14261e7395d334c59e406c74adb5a06c22044c9f
kevinys9208 <zrere@naver.com>
2022-07-20 12:16:40 +0900

    master

--- BreakTimeServer/src/main/java/indiv/park/breaktime/net/handler/NetHandler.java
+++ BreakTimeServer/src/main/java/indiv/park/breaktime/net/handler/NetHandler.java
@@ -5,11 +5,19 @@ import indiv.park.breaktime.game.object.Stage;
 import indiv.park.breaktime.net.NetChannelGroup;
 import indiv.park.network.server.annotation.ServerHandler;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.util.CharsetUtil;
+import io.netty.util.ReferenceCountUtil;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 
 @ServerHandler(group = "break", order = 3)
-public class NetHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+public class NetHandler extends ChannelInboundHandlerAdapter {
 	
 	@Override
 	public void channelActive(ChannelHandlerContext ctx) throws Exception {
@@ -22,9 +30,34 @@ public class NetHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> 
 	}
 
 	@Override
-	protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame frame) throws Exception {
-		String move = frame.text();
-		ControlType.valueOf(move).doControl(ctx.channel());
+	public void channelRead(ChannelHandlerContext ctx, Object obj) throws Exception {
+		if (obj instanceof FullHttpRequest) {
+			DefaultFullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+			response.content().writeCharSequence("Websocket connection allowed.", CharsetUtil.UTF_8);
+			
+			ctx.channel().writeAndFlush(response).addListener(new GenericFutureListener<Future<? super Void>>() {
+
+				@Override
+				public void operationComplete(Future<? super Void> future) throws Exception {
+					ctx.channel().close();
+				}
+			});
+		}
+		
+		if (obj instanceof TextWebSocketFrame) {
+			TextWebSocketFrame frame = null;
+			
+			try {
+				frame = (TextWebSocketFrame) obj;
+				
+				String move = frame.text();
+				ControlType.valueOf(move).doControl(ctx.channel());
+				
+			} finally {
+				if (frame != null)
+					ReferenceCountUtil.safeRelease(frame);
+			}
+		}
 	}
 	
 	@Override
